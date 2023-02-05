107066458df12885a696ac2cfbcf9bcc73e2e83c
antoniocenlaces <antoniocenlaces@gmail.com>
2023-02-05 20:29:45 +0000

    Probando desde Victus

--- codigo_pract0/listarArgumentos.cpp
+++ codigo_pract0/listarArgumentos.cpp
@@ -1,10 +1,11 @@
+// My version
 #include <iostream>
 
 using namespace std;
 
 // Pre: ---
 // Post: Presenta por la salida estandar un listado en el que se numeran
-//       los argumentos presentes en la linea de órdenes
+//       los argumentos presentes en la linea de ï¿½rdenes
 int main(int numArg, char* args[]) {
     for (int i = 0; i < numArg; ++i) {
         cout << i+1 << ". " << args[i] << endl;
