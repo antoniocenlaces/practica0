628696b90af110370d1d6655faf240edbc821edb
antoniocenlaces <antoniocenlaces@gmail.com>
2023-02-06 19:23:04 +0000

    Generar tabla ok

--- codigo_pract0/ordenar.cpp
+++ codigo_pract0/ordenar.cpp
@@ -45,3 +45,20 @@ void ordenar(int v[], const int n) {
     // Los datos de v[0..n-1] ya están ordenados
 }
 //--------------------------------------------------------------
+
+
+int values[] = { 40, 10, 100, 90, 20, 25 };
+
+int compare (const void * a, const void * b)
+{
+  return ( *(int*)a - *(int*)b );
+}
+
+int main ()
+{
+  int n;
+  qsort (values, 6, sizeof(int), compare);
+  for (n=0; n<6; n++)
+     printf ("%d ",values[n]);
+  return 0;
+}
--- entregables/generarTabla01.cpp
+++ entregables/generarTabla01.cpp
@@ -1,26 +1,34 @@
 #include <iostream>
 #include <iomanip>
 #include <ctime>
-#include <cstdlib>
+// #include <cstdlib>
 
 using namespace std;
 
 int randInt(const int a, const int b) {
-    srand(time(nullptr));
     return a + rand() % (b - a +1);
 }
 
 int main(int numArg, char* v[]) {
     if (numArg == 4){
+        srand(time(nullptr));
         int numeros = atoi(v[1]);
         int inicial = atoi(v[2]);
         int final = atoi(v[3]);
-        cout << "De nuevo Numeros: " << numeros << " Inicial: " << inicial << " final: " << final << endl;
-        unsigned i,j = 0;
-        while() {
-            while(j < 10) {
-
+        // cout << "De nuevo Numeros: " << numeros << " Inicial: " << inicial << " final: " << final << " args: " << numArg << endl;
+        // cout << "Prueba de randInt " << randInt(inicial, final) << endl;
+        unsigned i= 0;
+        unsigned j = 0;
+        while (i < numeros) {
+            // cout << "Entro en bucle 1; i=" << i<<endl;
+            while(j < 10 && i < numeros) {
+                // cout << "Entro en bucle 2; j=" << j<<endl;
+                cout << setw(8) << randInt(inicial, final);
+                j++;
+                i++;
             }
+            cout << endl;
+            j = 0;
         }
         return 0;
     } else
